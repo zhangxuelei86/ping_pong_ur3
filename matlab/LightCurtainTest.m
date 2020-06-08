1753b17d271d1df68dbf39118127d4fa8dd0eb79
fehinti <victorolufaj@gmail.com>
2020-06-09 03:44:29 +1000

    LightCurtain class complete
    The class for using the light curtain in the visualisation as a safety feature is complete and a test script setup.

---
+++ matlab/LightCurtainTest.m
@@ -0,0 +1,22 @@
+%% Test script for testing intersection with the Light Curtains
+function LightCurtainTest
+    close all;
+    clear all;
+    clc;
+
+    %% Create sources and then light curtain group
+    sources = [];
+    numSources = 1;
+    
+    for i = 1:numSources
+        sources(i).origin = rand(1,3) .* randi([-1 1],1,3);
+        sources(i).yaw = rand * 360;
+        sources(i).angDiff = 5;
+    end
+    
+    lightCurt = LightCurtain(sources,true);
+    
+    %% Test points of breach in Light curtain
+    breached = lightCurt.CheckBreach([0.4 0.8 0]);
+
+end
